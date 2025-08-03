c89e2064084355feed54acb36aec4ad5d4a4f54a
Gaby <gaby.es.sd@gmail.com>
2025-08-03 03:10:03 -0500

    Uploas periodic table project

--- periodictable/element.sh
+++ periodictable/element.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+# prompt to the user to provide an element as an argument
 if [[ ! $1 ]]
 then
   echo "Please provide an element as an argument."
+  exit
 else
   ELEMENT=$1
   if [[ $ELEMENT =~ ^[0-9]+$ ]]
   then
-    SELECT_ELEMENT_RESULT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number=$ELEMENT;")
+    SELECT_ELEMENT_RESULT=$($PSQL "SELECT atomic_number, name, symbol, types.type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number=$ELEMENT;")
   else
-    SELECT_ELEMENT_RESULT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE symbol='$ELEMENT' OR name='$ELEMENT';")
+    SELECT_ELEMENT_RESULT=$($PSQL "SELECT atomic_number, name, symbol, types.type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE symbol='$ELEMENT' OR name='$ELEMENT';")
   fi
-
   if [[ -z $SELECT_ELEMENT_RESULT ]]
   then
     echo "I could not find that element in the database."
@@ -20,13 +22,12 @@ else
     echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
   fi
 fi
-
-# DELETE ELEMENT
+# delete th non existent element whose atomi_number is 1000
 DELETE_PROPERTIE_RESULT=$($PSQL "DELETE FROM properties WHERE atomic_number=1000")
 if [[ $DELETE_PROPERTIE_RESULT == "DELETE 1" ]]
 then
   DELETE_ELEMENT_RESULT=$($PSQL "DELETE FROM elements WHERE atomic_number=1000")
 fi
 
-# DROP TYPE COLUMN FROM PROPERTIES TABLE
+# Delete type column from properties table
 DROP_TYPE_COLUMN=$($PSQL "ALTER TABLE properties DROP COLUMN IF EXISTS type;")
